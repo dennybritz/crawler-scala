a52544204845e38f259eb92645c3d70fd1929f57
Denny Britz <dennybritz@gmail.com>
2014-09-09 16:52:57 +0200

    Fix TestHttpServer routing

--- crawler-backend/src/test/scala/TestHttpServer.scala
+++ crawler-backend/src/test/scala/TestHttpServer.scala
@@ -16,21 +16,21 @@ import akka.util.Timeout
 class HttpServerListener extends HttpServiceActor with ActorLogging {
 
   def receive = runRoute {
-    path("/links" / IntNumber) { number =>
+    path("links" / IntNumber) { number =>
       val linkHtml : String = number match {
         case 1 => """<a href="/links/2">This is a link</a>"""
         case 2 => """<a href="/links/3">This is a link</a>"""
-        case 3 => ""
+        case _ => ""
       }
       complete(s"""<html><body>${linkHtml}</body></html>""")
     } ~ 
-    path("/status/301") { _ =>
+    path("status/301") { _ =>
       redirect("/", StatusCodes.PermanentRedirect)
     } ~
-    path("/status/404") { _ =>
+    path("status/404") { _ =>
       respondWithStatus(StatusCodes.NotFound)
     } ~
-    path("/status/503") { _ =>
+    path("status/503") { _ =>
       respondWithStatus(StatusCodes.ServiceUnavailable)
     } ~
     complete("")
