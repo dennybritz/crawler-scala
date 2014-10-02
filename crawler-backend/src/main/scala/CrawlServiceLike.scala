7a72f81458a5b4d3c0fd83e94ad268763b6717d2
Denny Britz <dennybritz@gmail.com>
2014-10-02 22:47:57 +0200

    Better logging statement

--- crawler-backend/src/main/scala/CrawlServiceLike.scala
+++ crawler-backend/src/main/scala/CrawlServiceLike.scala
@@ -79,7 +79,7 @@ trait CrawlServiceLike {
 
   /* Executes a FetchRequest using Spray's request-level library */
   def executeFetchRequest(fetchReq: FetchRequest) : Unit = {
-    log.info("fetching {}", fetchReq.req.uri.toString)
+    log.info("fetching url=\"{}\"", fetchReq.req.uri.toString)
     (IO(Http) ? fetchReq.req.req).mapTo[HttpResponse].map { res =>
       FetchResponse(fetchReq, res)
     } pipeTo responsePublisher
