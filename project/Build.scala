7a893e6a627d30307015b1b954175a85007409e8
Denny Britz <dennybritz@gmail.com>
2014-10-03 10:15:38 +0200

    Use default logging for crawler backend. Change sbt project names.

--- project/Build.scala
+++ project/Build.scala
@@ -6,16 +6,16 @@ object BlikkBuild extends Build {
   lazy val root = Project(id = "blikk", base = file(".")) aggregate(crawlerBackend, 
     crawlerLib, crawlerTest, exampleApp)
 
-  lazy val crawlerBackend = Project(id = "blikk-crawler-backend", base = file("./crawler-backend"),
+  lazy val crawlerBackend = Project(id = "crawler-backend", base = file("./crawler-backend"),
     settings = Project.defaultSettings ++ crawlerSettings) dependsOn(crawlerLib)
 
-  lazy val crawlerLib = Project(id="blikk-crawler-lib", base=file("./crawler-lib"), 
+  lazy val crawlerLib = Project(id="crawler-lib", base=file("./crawler-lib"), 
     settings = Project.defaultSettings ++ crawlerLibSettings)
 
-  lazy val crawlerTest = Project(id="blikk-crawler-tests", base=file("./crawler-test"), 
+  lazy val crawlerTest = Project(id="crawler-tests", base=file("./crawler-test"), 
     settings = Project.defaultSettings ++ crawlerTestSettings) dependsOn(crawlerLib, crawlerBackend)
 
-  lazy val exampleApp = Project(id="blikk-example-app", base=file("./example-app"),
+  lazy val exampleApp = Project(id="example-app", base=file("./example-app"),
     settings = Project.defaultSettings ++ exampleAppSettings) dependsOn(crawlerLib)
 
   val commonSettings = Seq(
@@ -29,7 +29,8 @@ object BlikkBuild extends Build {
   )
 
   val crawlerSettings = commonSettings ++ Seq(
-    name := "blikk-crawler-backend",
+    name := "crawler-backend",
+    envVars := Map("BLIKK_APP_NAME" -> "crawler-backend"),
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-cluster" % "2.3.5",
       "com.typesafe.akka" %% "akka-contrib" % "2.3.5",
@@ -38,12 +39,12 @@ object BlikkBuild extends Build {
   )
 
   val crawlerLibSettings = commonSettings ++ Seq(
-    name := "blikk-crawler-lib",
+    name := "crawler-lib",
     libraryDependencies ++= commonLibraryDependencies
   )
 
   val crawlerTestSettings = commonSettings ++ Seq(
-    name := "blikk-crawler-test",
+    name := "crawler-test",
     libraryDependencies ++= commonLibraryDependencies ++ Seq(
       "org.scalatest" %% "scalatest" % "2.2.1" % "test",
       "com.typesafe.akka" %% "akka-multi-node-testkit" % "2.3.6",
@@ -69,7 +70,7 @@ object BlikkBuild extends Build {
   )
 
   val exampleAppSettings = commonSettings ++ Seq(
-    name := "blikk-example-app"
+    name := "example-app"
   )
 
 }
